63985dd4074512c273a0db7371c0912c8be43c80
Lara <lara.durrant@gmail.com>
2019-10-02 21:30:34 -0700

    Add forward & back navigation, add links to main page

--- routes/photos.js
+++ routes/photos.js
@@ -62,6 +62,63 @@ router.get("/:id", function (req, res) {
     })
 });
 
+// next & prev route
+router.get("/:command/:index", function (req, res) {
+
+
+    var index = parseInt(req.params.index);
+
+    // If index is anyting other than a number, redirect back to the main gallery
+    if (!index || typeof (index) != 'number') {
+        res.redirect("/photos");
+    } else {
+
+        // Check the commands
+        // If it's anything other than prev or next, we'll use a positive index
+        if (req.params.command == "prev") {
+            index = index - 1;
+        } else if (req.params.command == "next") {
+            index = index + 1;
+        } else {
+            index = index + 1;
+        }
+
+        // attempt to find the total number of photos in the database
+        Photo.count({}, function (err, count) {
+
+            if (err || !count) {} else {
+                // if the count is greater than the max, reset the index counter
+                if (index >= count) {
+                    index = 0;
+                    // or if the index is less than zero, reset it to the max
+                } else if (index < 0) {
+                    index = count - 2;
+                }
+                // next, try to find a photo with that sort-index value
+                Photo.find({
+                    'sort-index': index
+                }, function (err, foundPhoto) {
+                    if (err || !foundPhoto) {
+                        res.redirect("/photos");
+                    } else {
+                        var devMode = req.app.locals.devMode;
+                        res.render("show", {
+                            photo: foundPhoto[0],
+                            devMode: devMode
+                        })
+                    }
+                })
+
+            }
+
+        })
+    }
+
+});
+
+
+
+
 //edit route
 
 router.get("/:id/edit", function (req, res) {
