9b64ef9b4f4a3df2241371d670c3868d224c1ff8
Lara <lara.durrant@gmail.com>
2019-10-06 09:55:23 -0700

    Add subdomain and contact page backend

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require("express");
 var router = express.Router({
     mergeParams: true
 });
+var nodemailer = require("nodemailer");
+var bodyParser = require("body-parser");
 
 var Photo = require('../models/photo');
+
+router.use(bodyParser.urlencoded({extended: true}))
 
 router.get("/", function (req, res) {
 
@@ -34,6 +38,43 @@ router.get("/about", function (req, res) {
 router.get("/contact", function (req, res) {
     res.render("contact");
 });
+
+
+// Nodemailer post route set up originally thanks to:
+// https://tylerkrys.ca/blog/adding-nodemailer-email-contact-form-node-express-app
+
+// POST route from contact form
+router.post('/contact', (req, res) => {
+
+    // Check out https://www.mailgun.com/ for a non-GMAIL e-mail
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'ressie.dibbert@ethereal.email',
+            pass: 'F3kJcqr2gjmJRpW7B5'
+        }
+    });
+  
+    // Specify what the email will look like
+    const mailOpts = {
+      from: req.body.email, // This is ignored by Gmail
+      to: 'ressie.dibbert@ethereal.email',
+      subject: `New contact from ${req.body.name} on Mom\'s site`,
+      text: req.body.message
+    }
+  
+    // Attempt to send the email
+    transporter.sendMail(mailOpts, (error, response) => {
+      if (error) {
+        res.render('contact-failure') // Show a page indicating failure
+      }
+      else {
+        console.log(response);
+        res.render('contact-success') // Show a page indicating success
+      }
+    })
+  })
 
 
 module.exports = router;
