e72442b90ecee9f18132633b9402a6b9825c5d04
Lara <lara.durrant@gmail.com>
2019-12-01 08:00:44 -0800

    Simplify routes

--- routes/photos.js
+++ routes/photos.js
@@ -16,7 +16,7 @@ router.get("/", function (req, res) {
 
 
     Photo.find({}).sort({
-        'sort-index': 1
+
     }).exec(function (err, photos) {
         if (err || !photos) {
             console.log("ERROR");
@@ -41,126 +41,64 @@ router.get("/new", isLoggedIn, function (req, res) {
 // create route
 router.post("/new", isLoggedIn, function (req, res) {
 
+    // uploads the file
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
 
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
 
-    // TODO: Refactor this code into a function
-    Photo.countDocuments({}, function (err, count) {
+    var filename = sampleFile.name.toUpperCase(sampleFile.name);
+    var filepath = 'public/img/gallery/' + filename;
+    var t_filepath = 'public/img/thumbs/t_' + filename;
 
-    
-        if (err || !count) {
-            res.send("Oops... I had a problem contacting the database.");
+    req.body['photo[image]'] = sampleFile.name.toUpperCase(sampleFile.name);
+
+    // the object we will pass to the database
+    var photoObject = {
+        title: req.body['photo[title]'],
+        tag: req.body['photo[tag]'],
+        description: req.body['photo[description]'],
+        color: req.body['photo[color]'],
+        image: filename
+    };
+
+    try {
+        if (fs.existsSync(filepath)) {
+            //file exists
+            res.send('Oops... File already exists!');
         } else {
+            // Use the mv() method to place the file somewhere on your server
+            sampleFile.mv(filepath, function (err) {
+                if (err)
+                    return res.status(500).send(err);
 
-            // gets the total count of the photos for the sort-index
-            // TODO: Instead of getting the count, let's instead sort by sort-index, get the highest index, and then set this new index to be that number
+                // TODO: Create an actual thumbnail here instead of just copy the original
+                sampleFile.mv(t_filepath, function (err) {
+                    if (err)
+                        return res.status(500).send(err);
 
-            req.body['photo[sort-index]'] = count;
+                    Photo.create(photoObject, function (err, newBlog) {
+                        if (err || !newBlog) {
+                            res.render("new");
+                        } else {
+                            res.redirect("/photos");
+                        }
+                    })
 
-            // uploads the file
-            if (!req.files || Object.keys(req.files).length === 0) {
-                return res.status(400).send('No files were uploaded.');
-            }
+                });
 
-            // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-            let sampleFile = req.files.sampleFile;
-            var name = sampleFile.name.toUpperCase(sampleFile.name);
-            req.body['photo[image]'] = name;
-            var filepath = 'public/img/gallery/' + name;
-            var t_filepath = 'public/img/thumbs/t_' + name;
-            console.log(filepath);
-
-            try {
-                if (fs.existsSync(filepath)) {
-                    //file exists
-                    res.send('Oops... File already exists!');
-                } else {
-                    // Use the mv() method to place the file somewhere on your server
-                    sampleFile.mv(filepath, function (err) {
-                        if (err)
-                            return res.status(500).send(err);
-
-                        // TODO: Create an actual thumbnail here instead of just copy the original
-                        sampleFile.mv(t_filepath, function (err) {
-                            if (err)
-                                return res.status(500).send(err);
-
-                            var photoObject = {
-                                'sort-index': count,
-                                title: req.body['photo[title]'],
-                                tag: req.body['photo[tag]'],
-                                description: req.body['photo[description]'],
-                                color: req.body['photo[color]'],
-                                image: name
-                            };
-
-                            console.log(photoObject);
-
-                            Photo.create(photoObject, function (err, newBlog) {
-                                if (err || !newBlog) {
-                                    res.render("new");
-                                } else {
-                                    res.redirect("/photos");
-                                }
-                            })
-
-                        });
-
-                    });
-
-                }
-            } catch (err) {
-                console.error(err)
-            }
-
-
+            });
 
         }
-    })
+    } catch (err) {
+        console.error(err)
+    }
+
 
 });
 
-
-
-/*
-router.get("/upload", function(req, res){
-    res.render("upload");
-})
-
-router.post("/upload", function(req, res){
-
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-      }
-    
-      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let sampleFile = req.files.sampleFile;
-
-      var name = sampleFile.name.toUpperCase(sampleFile.name);
-      var filepath = 'public/img/gallery/IMG_' + name;
-
-      console.log(filepath);
-
-      try {
-        if (fs.existsSync(filepath)) {
-          //file exists
-          res.send('File already exists!');
-        }
-        else{
-            // Use the mv() method to place the file somewhere on your server
-            sampleFile.mv(filepath, function(err) {
-            if (err)
-                return res.status(500).send(err);
-            
-                res.send('File uploaded!');
-            });
-            }
-      } catch(err) {
-        console.error(err)
-      }
-
-
-})
-*/
 
 
 // show route
@@ -178,63 +116,6 @@ router.get("/:id", function (req, res) {
         }
     })
 });
-
-// next & prev route
-router.get("/ss/:command/:index", function (req, res) {
-
-
-    var index = parseInt(req.params.index);
-
-    // If index is anyting other than a number, redirect back to the main gallery
-    if (index == undefined || typeof (index) != 'number') {
-
-        res.redirect("/photos");
-    } else {
-
-        // Check the commands
-        // If it's anything other than prev or next, we'll use a positive index
-        if (req.params.command == "prev") {
-            index = index - 1;
-        } else if (req.params.command == "next") {
-            index = index + 1;
-        } else {
-            index = index + 1;
-        }
-
-        // attempt to find the total number of photos in the database
-        Photo.countDocuments({}, function (err, count) {
-
-            if (err || !count) {} else {
-                // if the count is greater than the max, reset the index counter
-                if (index >= count) {
-                    index = 0;
-                    // or if the index is less than zero, reset it to the max
-                } else if (index < 0) {
-                    index = count - 2;
-                }
-                // next, try to find a photo with that sort-index value
-                Photo.find({
-                    'sort-index': index
-                }, function (err, foundPhoto) {
-                    if (err || !foundPhoto) {
-                        res.redirect("/photos");
-                    } else {
-
-                        res.render("show", {
-                            photo: foundPhoto[0]
-
-                        })
-                    }
-                })
-
-            }
-
-        })
-    }
-
-});
-
-
 
 
 //edit route
